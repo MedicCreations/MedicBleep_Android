de94f3fadbe7317c26f1127435bded440a7be65d
Matej Vida <vida@Matejs-iMac.local>
2014-07-03 09:16:20 +0200

    removed background

--- RandomApp/src/com/example/gamevector/MainActivity.java
+++ RandomApp/src/com/example/gamevector/MainActivity.java
@@ -10,30 +10,30 @@ import android.widget.Button;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-    Button go;
+	Button go;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-	go = (Button) findViewById(R.id.go);
-	go.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-	switch (view.getId()) {
-	case R.id.go:
-
-	    Intent intent = new Intent(this, CharacterListActivity.class);
-	    startActivity(intent);
-
-	    break;
-
-	default:
-	    break;
+		go = (Button) findViewById(R.id.go);
+		go.setOnClickListener(this);
 	}
-    }
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.go:
+
+			Intent intent = new Intent(this, CharacterListActivity.class);
+			startActivity(intent);
+
+			break;
+
+		default:
+			break;
+		}
+	}
 
 }
