8af2297f43426e493219276d63f4716341584064
Matej Vida <matej.vida@clover-studio.com>
2014-07-16 17:10:42 +0200

    Api package

--- SpikaEnterprise/src/com/clover/spika/enterprise/chat/api/LoginApi.java
+++ SpikaEnterprise/src/com/clover/spika/enterprise/chat/api/LoginApi.java
@@ -1,5 +1,32 @@
 package com.clover.spika.enterprise.chat.api;
 
+import android.content.Context;
+
+import com.clover.spika.enterprise.chat.extendables.BaseAsyncTask;
+
 public class LoginApi {
 
+	public static void loginWithCredentials(String username, String password, final LoginListener listener, Context ctx, boolean showProgressBar) {
+
+		new BaseAsyncTask<Void, Void, Void>(ctx, showProgressBar) {
+
+			protected Void doInBackground(Void... params) {
+				return null;
+			};
+
+			protected void onPostExecute(Void result) {
+
+				if (listener != null) {
+					listener.onLogin();
+				}
+			};
+
+		}.execute();
+	}
+
+	interface LoginListener {
+		void onLogin();
+
+		void onLoginFailed();
+	}
 }
