352973eb8836f7ade83ce330326ed428bdc9c158
MislavBagovic <mislav.bagovic@clover-studio.com>
2014-10-06 19:03:20 +0200

    Novi RoundImageView (Fix problema sa memorijom na S3)

--- SpikaEnterprise/src/com/clover/spika/enterprise/chat/views/RoundImageView.java
+++ SpikaEnterprise/src/com/clover/spika/enterprise/chat/views/RoundImageView.java
@@ -1,44 +1,12 @@
 package com.clover.spika.enterprise.chat.views;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
-import android.widget.ImageView;
 
-import com.clover.spika.enterprise.chat.utils.Helper;
-
-public class RoundImageView extends ImageView {
-
-	public RoundImageView(Context context) {
-		super(context);
-	}
+public class RoundImageView extends RoundedImageView {
 
 	public RoundImageView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-	}
-
-	public RoundImageView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
-
-	@Override
-	protected void onDraw(Canvas canvas) {
-		if (isDrawValid()) {
-			Bitmap b = ((BitmapDrawable) getDrawable()).getBitmap();
-			Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
-
-			int width = getWidth();
-			// int height = getHeight();
-
-			int paddingLeft = getPaddingLeft();
-
-			canvas.drawBitmap(Helper.getRoundedBitmap(bitmap, width - paddingLeft), paddingLeft / 2, paddingLeft / 2, null);
-		}
-	}
-
-	private boolean isDrawValid() {
-		return getDrawable() != null && getDrawable() instanceof BitmapDrawable && getWidth() > 0 && getHeight() > 0;
+		this.setOval(true);
 	}
 }
