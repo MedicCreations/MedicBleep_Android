3210e55cdc3f9693003b6e059c6798511d3e4dc5
Matej Vida <matej.vida@clover-studio.com>
2015-03-26 15:32:11 +0100

    Caching work

--- SpikaEnterprise/src/com/clover/spika/enterprise/chat/caching/LobbyCaching.java
+++ SpikaEnterprise/src/com/clover/spika/enterprise/chat/caching/LobbyCaching.java
@@ -158,59 +158,226 @@ public class LobbyCaching {
 					com.clover.spika.enterprise.chat.models.greendao.Organization finalOrganizationModel = null;
 					if (chat.user.organization != null) {
 
-						finalOrganizationModel = new com.clover.spika.enterprise.chat.models.greendao.Organization((Long.valueOf(chat.user.organization.id)),
-								chat.user.organization.name);
+						if (organizationDao.queryBuilder().where(com.clover.spika.enterprise.chat.models.greendao.OrganizationDao.Properties.Id.eq(chat.user.organization.id))
+								.count() > 0) {
 
-						organizationDao.insertOrReplace(finalOrganizationModel);
+							finalOrganizationModel = organizationDao.queryBuilder()
+									.where(com.clover.spika.enterprise.chat.models.greendao.OrganizationDao.Properties.Id.eq(chat.user.organization.id)).unique();
+
+							if (chat.user.organization.name != null) {
+								finalOrganizationModel.setName(chat.user.organization.name);
+							}
+
+							organizationDao.update(finalOrganizationModel);
+
+						} else {
+							finalOrganizationModel = new com.clover.spika.enterprise.chat.models.greendao.Organization((Long.valueOf(chat.user.organization.id)),
+									chat.user.organization.name);
+							organizationDao.insert(finalOrganizationModel);
+						}
 					}
 
-					com.clover.spika.enterprise.chat.models.greendao.User finalUserModel = new com.clover.spika.enterprise.chat.models.greendao.User((long) chat.user.id,
-							(long) chat.user.user_id, chat.user.firstname, chat.user.lastname, chat.user.type, chat.user.image, chat.user.image_thumb, chat.user.is_member,
-							chat.user.is_admin, chat.user.name, chat.user.groupname, chat.user.chat_id, chat.user.is_user, chat.user.is_group, chat.user.is_room);
+					if (userDao.queryBuilder().where(com.clover.spika.enterprise.chat.models.greendao.UserDao.Properties.Id.eq(chat.user.organization.id)).count() > 0) {
 
-					// TODO
-					userDao.insertOrReplace(finalUserModel);
-					finalUserModelId = finalUserModel.getId();
+						com.clover.spika.enterprise.chat.models.greendao.User finalUserModel = userDao.queryBuilder()
+								.where(com.clover.spika.enterprise.chat.models.greendao.UserDao.Properties.Id.eq(chat.user.organization.id)).unique();
+						
+						
+						finalUserModel.setId((long) chat.user.id);
+						
+						if(chat.user.user_id != 0){
+							
+						}
+//						finalUserModel
+//						
+//						(long) chat.user.user_id,
+//						chat.user.firstname,
+//						chat.user.lastname,
+//						chat.user.type,
+//						chat.user.image,
+//						chat.user.image_thumb,
+//						chat.user.is_member,
+//						chat.user.is_admin,
+//						chat.user.name,
+//						chat.user.groupname,
+//						chat.user.chat_id,
+//						chat.user.is_user,
+//						chat.user.is_group,
+//						chat.user.is_room
+
+						userDao.update(finalUserModel);
+						finalUserModelId = finalUserModel.getId();
+						
+					} else {
+						
+						com.clover.spika.enterprise.chat.models.greendao.User finalUserModel = new com.clover.spika.enterprise.chat.models.greendao.User((long) chat.user.id,
+								(long) chat.user.user_id, chat.user.firstname, chat.user.lastname, chat.user.type, chat.user.image, chat.user.image_thumb, chat.user.is_member,
+								chat.user.is_admin, chat.user.name, chat.user.groupname, chat.user.chat_id, chat.user.is_user, chat.user.is_group, chat.user.is_room);
+
+						userDao.insert(finalUserModel);
+						finalUserModelId = finalUserModel.getId();
+					}
 				}
 
 				Long finalMessageModelId = 0L;
 				if (chat.last_message != null) {
-					com.clover.spika.enterprise.chat.models.greendao.Message finalMessageModel = new com.clover.spika.enterprise.chat.models.greendao.Message(
-							Long.valueOf(chat.last_message.id), Long.valueOf(chat.last_message.chat_id), Long.valueOf(chat.last_message.user_id), chat.last_message.firstname,
-							chat.last_message.lastname, chat.last_message.image, chat.last_message.text, chat.last_message.file_id, chat.last_message.thumb_id,
-							chat.last_message.longitude, chat.last_message.latitude, chat.last_message.created, chat.last_message.modified, chat.last_message.child_list,
-							chat.last_message.image_thumb, chat.last_message.type, chat.last_message.root_id, chat.last_message.parent_id, chat.last_message.isMe,
-							chat.last_message.isFailed, (long) chat.chat_id);
 
-					// TODO
-					messageDao.insertOrReplace(finalMessageModel);
-					finalMessageModelId = finalMessageModel.getId();
+					if (messageDao.queryBuilder().where(com.clover.spika.enterprise.chat.models.greendao.MessageDao.Properties.Id.eq(chat.last_message.id)).count() > 0) {
+
+						com.clover.spika.enterprise.chat.models.greendao.Message finalMessageModel = messageDao.queryBuilder()
+								.where(com.clover.spika.enterprise.chat.models.greendao.MessageDao.Properties.Id.eq(chat.last_message.id)).unique();
+
+						finalMessageModel.setId(Long.valueOf(chat.last_message.id));
+
+						if (chat.last_message.chat_id != null && Long.valueOf(chat.last_message.chat_id) != 0L) {
+							finalMessageModel.setChat_id(Long.valueOf(chat.last_message.chat_id));
+						}
+
+						if (chat.last_message.user_id != null && Long.valueOf(chat.last_message.user_id) != 0L) {
+							finalMessageModel.setUser_id(Long.valueOf(chat.last_message.user_id));
+						}
+
+						if (chat.last_message.firstname != null) {
+							finalMessageModel.setFirstname(chat.last_message.firstname);
+						}
+
+						if (chat.last_message.lastname != null) {
+							finalMessageModel.setLastname(chat.last_message.lastname);
+						}
+
+						if (chat.last_message.text != null) {
+							finalMessageModel.setText(chat.last_message.text);
+						}
+
+						if (chat.last_message.file_id != null) {
+							finalMessageModel.setFile_id(chat.last_message.file_id);
+						}
+
+						if (chat.last_message.thumb_id != null) {
+							finalMessageModel.setThumb_id(chat.last_message.thumb_id);
+						}
+
+						if (chat.last_message.longitude != null) {
+							finalMessageModel.setLongitude(chat.last_message.longitude);
+						}
+
+						if (chat.last_message.latitude != null) {
+							finalMessageModel.setLongitude(chat.last_message.latitude);
+						}
+
+						if (chat.last_message.created != null) {
+							finalMessageModel.setCreated(chat.last_message.created);
+						}
+
+						if (chat.last_message.modified != null) {
+							finalMessageModel.setModified(chat.last_message.modified);
+						}
+
+						if (chat.last_message.child_list != null) {
+							finalMessageModel.setChild_list(chat.last_message.child_list);
+						}
+
+						if (chat.last_message.image_thumb != null) {
+							finalMessageModel.setImage_thumb(chat.last_message.image_thumb);
+						}
+
+						if (chat.last_message.image_thumb != null) {
+							finalMessageModel.setImage_thumb(chat.last_message.image_thumb);
+						}
+
+						if (chat.last_message.type != 0) {
+							finalMessageModel.setType(chat.last_message.type);
+						}
+
+						if (chat.last_message.root_id != 0) {
+							finalMessageModel.setRoot_id(chat.last_message.root_id);
+						}
+
+						if (chat.last_message.parent_id != 0) {
+							finalMessageModel.setParent_id(chat.last_message.parent_id);
+						}
+
+						finalMessageModel.setIsMe(chat.last_message.isMe);
+						finalMessageModel.setIsFailed(chat.last_message.isFailed);
+
+						if (chat.chat_id != 0) {
+							finalMessageModel.setParent_id(chat.chat_id);
+						}
+
+					} else {
+						com.clover.spika.enterprise.chat.models.greendao.Message finalMessageModel = new com.clover.spika.enterprise.chat.models.greendao.Message(
+								Long.valueOf(chat.last_message.id), Long.valueOf(chat.last_message.chat_id), Long.valueOf(chat.last_message.user_id), chat.last_message.firstname,
+								chat.last_message.lastname, chat.last_message.image, chat.last_message.text, chat.last_message.file_id, chat.last_message.thumb_id,
+								chat.last_message.longitude, chat.last_message.latitude, chat.last_message.created, chat.last_message.modified, chat.last_message.child_list,
+								chat.last_message.image_thumb, chat.last_message.type, chat.last_message.root_id, chat.last_message.parent_id, chat.last_message.isMe,
+								chat.last_message.isFailed, (long) chat.chat_id);
+
+						messageDao.insertOrReplace(finalMessageModel);
+						finalMessageModelId = finalMessageModel.getId();
+					}
 				}
 
-				// TODO
 				if (chatDao.queryBuilder().where(Properties.Chat_id.eq(chat.chat_id)).count() > 0) {
+
 					com.clover.spika.enterprise.chat.models.greendao.Chat usedChatModel = chatDao.queryBuilder().where(Properties.Chat_id.eq(chat.chat_id)).unique();
 
 					usedChatModel.setChat_id(Long.valueOf(chat.chat_id));
-					usedChatModel.setId(Long.valueOf(chat.chat_id));
-					usedChatModel.setChat_name(chat.chat_name);
-					usedChatModel.setSeen_by(chat.seen_by);
-					if((Integer)chat.total_count != null || chat.total_count != 0){
+					usedChatModel.setId(Long.valueOf(chat.id));
+
+					if (chat.chat_name != null) {
+						usedChatModel.setChat_name(chat.chat_name);
+					}
+
+					if (chat.seen_by != null) {
+						usedChatModel.setSeen_by(chat.seen_by);
+					}
+
+					if ((Integer) chat.total_count != null || chat.total_count != 0) {
 						usedChatModel.setTotal_count(chat.total_count);
 					}
-					usedChatModel.setImage_thumb(chat.image_thumb);
-					usedChatModel.setImage(chat.image);
-					usedChatModel.setAdmin_id(chat.image);
+
+					if (chat.image_thumb != null) {
+						usedChatModel.setImage_thumb(chat.image_thumb);
+					}
+
+					if (chat.image != null) {
+						usedChatModel.setImage(chat.image);
+					}
+
+					if (chat.admin_id != null) {
+						usedChatModel.setAdmin_id(chat.admin_id);
+					}
+
 					usedChatModel.setIs_active(chat.is_active);
 					usedChatModel.setType(chat.type);
 					usedChatModel.setIs_private(chat.is_private);
-					usedChatModel.setPassword(chat.password);
-					usedChatModel.setUnread(chat.unread);
+
+					if (chat.password != null) {
+						usedChatModel.setPassword(chat.password);
+					}
+
+					if (chat.unread != null) {
+						usedChatModel.setUnread(chat.unread);
+					}
+
 					usedChatModel.setIs_member(chat.is_member);
-					usedChatModel.setModified(chat.modified);
-					usedChatModel.setCategoryId(finalCategoryModelId);
-					usedChatModel.setUserIdProperty(finalUserModelId);
-					usedChatModel.setMessageIdProperty(finalMessageModelId);
+
+					if (chat.modified != 0L) {
+						usedChatModel.setModified(chat.modified);
+					}
+
+					if (finalCategoryModelId != 0L) {
+						usedChatModel.setCategoryId(finalCategoryModelId);
+					}
+
+					if (finalUserModelId != 0L) {
+						usedChatModel.setUserIdProperty(finalUserModelId);
+					}
+
+					if (finalMessageModelId != 0L) {
+						usedChatModel.setMessageIdProperty(finalMessageModelId);
+					}
+
 					usedChatModel.setIsRecent(true);
 
 					chatDao.update(usedChatModel);
