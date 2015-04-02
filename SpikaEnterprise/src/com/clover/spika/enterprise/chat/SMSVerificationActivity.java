a2327c973d48f8adff222f57426f2b4784a94f5f
MislavBagovic <mislav.bagovic@clover-studio.com>
2015-04-02 17:34:59 +0200

    phone number verification

--- SpikaEnterprise/src/com/clover/spika/enterprise/chat/SMSVerificationActivity.java
+++ SpikaEnterprise/src/com/clover/spika/enterprise/chat/SMSVerificationActivity.java
@@ -1,24 +1,41 @@
 package com.clover.spika.enterprise.chat;
 
+import java.util.Locale;
+
 import com.clover.spika.enterprise.chat.utils.Const;
 import com.clover.spika.enterprise.chat.utils.PasscodeUtility;
 import com.clover.spika.enterprise.chat.views.RobotoThinEditText;
+import com.clover.spika.enterprise.chat.views.RobotoThinTextView;
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
+import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class SMSVerificationActivity extends Activity {
 
+	public static final String PHONE_NUMBER = "phone_number";
+	
 	public static final int TYPE_PHONE_NUMBER = 0;
 	public static final int TYPE_VERIFICATION_CODE = 1;
 	
 	private int type;
 	
 	RobotoThinEditText editText;
+	RobotoThinTextView textView;
+	String phoneNumber;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +43,28 @@ public class SMSVerificationActivity extends Activity {
 		setContentView(R.layout.activity_sms_verfication);
 		
 		editText = (RobotoThinEditText)findViewById(R.id.inputEditText);
+		textView = (RobotoThinTextView)findViewById(R.id.phoneNumberTextView);
 		
 		type = getIntent().getIntExtra(Const.TYPE, TYPE_PHONE_NUMBER);
 		if (type == TYPE_PHONE_NUMBER) {
 			findViewById(R.id.changePhoneNumberButton).setVisibility(View.GONE);
-			findViewById(R.id.phoneNumberTextView).setVisibility(View.GONE);
+			textView.setVisibility(View.GONE);
 			editText.setHint(R.string.phone_number);
+			editText.setOnEditorActionListener(new OnEditorActionListener() {
+				@Override
+				public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+					if (actionId == EditorInfo.IME_ACTION_DONE) {
+						checkPhoneNumber();
+					}
+					return false;
+				}
+			});
+			editText.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					editText.setBackgroundColor(Color.WHITE);
+				}
+			});
 		}
 		else {
 			Button changePhoneNumberButton = (Button)findViewById(R.id.changePhoneNumberButton);
@@ -45,6 +78,9 @@ public class SMSVerificationActivity extends Activity {
 			});
 			findViewById(R.id.phoneNumberTextView).setVisibility(View.VISIBLE);
 			editText.setHint(R.string.verification_code);
+			textView.setVisibility(View.VISIBLE);
+			phoneNumber = getIntent().getStringExtra(PHONE_NUMBER);
+			textView.setText(getString(R.string.sms_sent_to) + phoneNumber);
 		}
 		
 		Button submitButton = (Button) findViewById(R.id.submitButton);
@@ -62,9 +98,12 @@ public class SMSVerificationActivity extends Activity {
 	}
 	
 	void submitPhoneNumberAPI () {
-		Intent intent = new Intent(this, SMSVerificationActivity.class);
-		intent.putExtra(Const.TYPE, SMSVerificationActivity.TYPE_VERIFICATION_CODE);
-		startActivityForResult(intent, Const.REQUEST_VERIFICATION_CODE);
+		if (checkPhoneNumber()) {
+			Intent intent = new Intent(this, SMSVerificationActivity.class);
+			intent.putExtra(Const.TYPE, SMSVerificationActivity.TYPE_VERIFICATION_CODE);
+			intent.putExtra(PHONE_NUMBER, editText.getText().toString());
+			startActivityForResult(intent, Const.REQUEST_VERIFICATION_CODE);
+		}
 	}
 	
 	void submitVerificationCodeAPI () {
@@ -95,5 +134,37 @@ public class SMSVerificationActivity extends Activity {
 		else {
 			super.onBackPressed();
 		}
+	}
+	
+	boolean checkPhoneNumber () {
+		boolean result = false;
+		
+		PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+		PhoneNumber phoneNumber;
+
+		String defaultLocale = Locale.getDefault().getCountry();
+
+		String phoneNumberToCheck = editText.getText().toString();
+
+		if (phoneNumberToCheck.startsWith("00")) {
+			phoneNumberToCheck = phoneNumberToCheck.replaceFirst("00", "+");
+		}
+		
+		try {
+			phoneNumber = phoneUtil.parse(phoneNumberToCheck, defaultLocale);
+			if (phoneUtil.isValidNumber(phoneNumber)) {
+				editText.setText(phoneUtil.format(phoneNumber, PhoneNumberFormat.INTERNATIONAL));
+				editText.setBackgroundColor(Color.GREEN);
+				result = true;
+			} else {
+				editText.setBackgroundColor(Color.RED);
+				result = false;
+			}
+		} catch (NumberParseException e) {
+			e.printStackTrace();
+			editText.setBackgroundColor(Color.RED);
+			result = false;
+		} 
+		return result;
 	}
 }
